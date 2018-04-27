b65535dbdbb4173ebea542093e3ade558854eded
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-04-27 17:36:19 +0200

    src/exon_class.py - Creation of an intron class and an get_iupac_exon method for the ExonClass class. Modification of the Gene class init method (not cnx required now) + modification of exon calss main for it method and it attribute take in account the new Gene class

--- Database_creator/src/exon_class.py
+++ Database_creator/src/exon_class.py
@@ -26,7 +26,7 @@ class ExonClassMain:
         :param gene_id: (int) the id of a gene
         :param exon_position:  (int) the position of the exon on the gene
         """
-        self.gene = Gene(cnx, gene_name, gene_id)
+        self.gene = Gene(gene_name, gene_id)
         self.position = exon_position
         length, exon_type, donor, acceptor = self.get_exon_info(cnx)
         self.length = length
@@ -45,13 +45,13 @@ class ExonClassMain:
         query = """
         SELECT end_on_gene - start_on_gene + 1, exon_type, force_donor, force_acceptor
         FROM exons
-        WHERE id_gene = \"""" + str(self.gene_id) + """\"
+        WHERE id_gene = \"""" + str(self.gene.id) + """\"
         AND pos_on_gene = """ + str(self.position) + """ ;
         """
         cursor.execute(query)
         result = cursor.fetchone()
         if result is None:
-            return (None, None, None, None)
+            return None, None, None, None
         return result
 
 
@@ -69,19 +69,46 @@ class ExonClass(ExonClassMain):
         :param exon_position:  (int) the position of the exon on the gene
         """
         ExonClassMain.__init__(self, cnx, gene_name, gene_id, exon_position)
+        self.gene.gene_filler(cnx)
         self.upstream_exon = ExonClassMain(cnx, gene_name, gene_id, exon_position - 1)
         self.downstream_exon = ExonClassMain(cnx, gene_name, gene_id, exon_position + 1)
+        self.upstream_intron = Intron(cnx, gene_id, self.position - 1, self.gene.sequence, "upstream")
+        self.downstream_intron = Intron(cnx, gene_id, self.position - 1, self.gene.sequence, "downstream")
+        self.iupac = self.get_iupac_exon(cnx)
+        # once the exon is fully created we delete the gene sequence for memory efficiency
+        self.gene.sequence = None
+
+    def get_iupac_exon(self, cnx):
+        """
+        Get the iupac content of the exon
+        :return: (string) the iupac content of the exon
+        """
+        cursor = cnx.cursor()
+        query = """SELECT start_on_gene, end_on_gene
+                   FROM exons
+                   WHERE id_gene = """ + self.gene.id + """
+                   AND pos_on_gene = """ + self.position + ";"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if len(result) > 1:
+            print("More than one exon was retrieve with a sing exon id")
+            print("Exiting...")
+            exit(1)
+        start = result[0][0] - 1
+        stop = result[0][1]
+        sequence = self.gene.sequence[start:stop]
+        iupac = iupac_frequencies(sequence)
+        return ";".join(list(map(str, iupac)))
 
 
 class Gene:
     """
     Contains every info of interest for a gene
     """
-    def __init__(self, cnx, gene_name, gene_id):
+    def __init__(self, gene_name, gene_id):
         """
         Initiate the creation of a gene
 
-        :param cnx: (sqlite3 object) connection to fasterDB Lite
         :param gene_name: (string) official symbol for an exons
         :param gene_id: (int) the id of the gene
         """
@@ -91,6 +118,7 @@ class Gene:
         self.median_intron_size = None
         self.iupac = None
         self.length = None
+        self.sequence = None
 
     def get_iupac_and_gene_length(self, cnx):
         """
@@ -101,14 +129,15 @@ class Gene:
         cursor = cnx.cursor()
         query = """SELECT sequence
                    FROM genes
-                   WHERE id = """ + self.id + """;"""
+                   WHERE id = """ + str(self.id) + """;"""
         cursor.execute(query)
         result = cursor.fetchall()
         if len(result) > 1:
-            print("Multiple sequence were retrive from a single gene id")
+            print("Multiple sequence were retrieve from a single gene id")
             print("Exiting...")
             exit(1)
         sequence = result[0][0]
+        self.sequence = sequence
         self.length = len(sequence)
         iupac = iupac_frequencies(sequence)
         res = ";".join(list(map(str, iupac)))
@@ -123,7 +152,7 @@ class Gene:
         cursor = cnx.cursor()
         query = """SELECT end_on_gene - start_on_gene + 1
                     FROM introns
-                    WHERE id_gene = """ + self.id + ";"
+                    WHERE id_gene = """ + str(self.id) + ";"
         cursor.execute(query)
         result = cursor.fetchall()
         self.nb_intron = len(result)
@@ -138,6 +167,65 @@ class Gene:
         """
         self.get_iupac_and_gene_length(cnx)
         self.get_nb_intron_and_median_intron_size(cnx)
+
+
+class Intron:
+    """Create an intron"""
+    def __init__(self, cnx, gene_id, pos_on_gene, gene_sequence, location):
+        """
+
+        :param cnx: (sqlite3 object) allow connection to **FasterDB Lite** database
+        :param gene_id:  (int) the id of a gene
+        :param pos_on_gene: (int) an intron position on a gene
+        :param gene_sequence: (string) the sequence of the gene, gene_id
+        :param location: (string) upstream or downstream
+        """
+        self.gene_id = gene_id
+        self.position = pos_on_gene
+        self.location = location
+        length, iupac_proxi, iupac_dist = self.get_intron_information(cnx, gene_sequence)
+        self.length = length
+        self.iupac_proxi = iupac_proxi
+        self.iupac_dist = iupac_dist
+
+    def get_intron_information(self, cnx, gene_seq):
+        """
+        Get the intron size and the iupac composition of distal and proximal sequence
+
+        :param cnx: (sqlite3 object) allows connection to **FasterDB Lite** database
+        :param gene_seq: (string) the sequence of the gene, gene_id
+        result :
+            - length: (int) intron length
+            - proximal: (string) the proximal (0:25) frequency in the intron
+            - distal: (string)  the proximal (26:100) iupac frequency in the intorn
+        """
+        cursor = cnx.cursor()
+        query = """SELECT start_on_gene, end_on_gene
+                   FROM introns
+                   WHERE id_gene = """ + str(self.gene_id) + """
+                   AND pos_on_gene = """ + str(self.position) + ";"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if len(result) > 1:
+            print("Error : multiple intron find from a simple intron id")
+            print("exiting...")
+            exit(1)
+        start = result[0][0] - 1
+        end = result[0][1]
+        sequence = gene_seq[start:end]
+        if self.location == "upstream":
+            sequence = sequence[::-1]
+        proxi_seq = sequence[0:25]
+        distal_seq = sequence[26:100]
+        if len(proxi_seq) > 0:
+            iupac_poxi = ";".join(list(map(str, iupac_frequencies(proxi_seq))))
+        else:
+            iupac_poxi = None
+        if len(distal_seq) > 0:
+            iupac_dist = ";".join(list(map(str, iupac_frequencies(distal_seq))))
+        else:
+            iupac_dist = None
+        return len(sequence), iupac_poxi, iupac_dist
 
 
 # simple function for getting iupac frequencies
@@ -158,4 +246,3 @@ def iupac_frequencies(sequence):
             result.append(round((float(sequence.count(iupac[nt][0]) +
                                        sequence.count(iupac[nt][1])) / len(sequence)) * 100, 1))
     return result
-

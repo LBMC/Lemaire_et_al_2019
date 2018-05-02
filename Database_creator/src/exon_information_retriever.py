8d78655b1c1f53102c978b54226d41fb73c85082
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-05-02 16:11:52 +0200

    src/exon_information_retriever.py - modification of get_exon_info to show progression, creation of sed_connection to connect sed database, creation of create_sed_exon_table to create sed table, creation of sed_filler to fill sed table and modification of main to take in account the new created functions

--- Database_creator/src/exon_information_retriever.py
+++ Database_creator/src/exon_information_retriever.py
@@ -6,16 +6,13 @@
 
 import exon_class
 import sqlite3
-import os
 from database_creator import base_name, out_path
-# debug mode
-debug = 1 # 1 = enabled , 0 disabled
-
+import sys
 
 # functions
 def fasterdbl_connection():
     """
-    :param file_name: (string) file where the database will be created
+    Allow connection to fasterDB Lite
     """
     return sqlite3.connect(out_path + base_name)
 
@@ -37,7 +34,7 @@ def exon_finder(cnx):
                FROM genes t2, exons t1
                WHERE t2.id = t1.id_gene"""
     cursor.execute(query)
-    return(cursor.fetchall()[0:3])
+    return cursor.fetchall()
 
 
 def get_exon_info(cnx, info_list, debug):
@@ -50,10 +47,17 @@ def get_exon_info(cnx, info_list, debug):
     a string : gene_symbol and 2 int : the gene_id and the exobn position on gene respectively
     :return: (a list of ExonClass object) list of exons
     """
+    print("Getting exons information !")
     exon_list = []
     exon_class.set_debug(debug)
+    count = 0
+    ll = str(len(info_list))
     for exon_info in info_list:
         exon_list.append(exon_class.ExonClass(cnx, exon_info[0], exon_info[1], exon_info[2]))
+        count += 1
+        percent = round(float(count) / len(info_list) * 100, 1)
+        sys.stdout.write("Progression : " + str(count) + " / " + ll + " - " + str(percent) + " %\r")
+        sys.stdout.flush()
     return exon_list
 
 
@@ -95,16 +99,78 @@ def write_result(list_tuple):
             outfile.write(str(mytuple).replace("[", "").replace("]", "").replace(", ", "\t") + "\n")
 
 
+def sed_connection():
+    """
+    Connection to SED database
+    """
+    return sqlite3.connect(out_path + "sed.db")
+
+
+def create_sed_exon_table(sed_cnx):
+    """
+    :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
+    SED database.
+    """
+    cursor = sed_cnx.cursor()
+    query = """CREATE TABLE sed (
+               gene_symbol VARCHAR(17) NOT NULL,
+               gene_id INT(10) NOT NULL,
+               exon_pos INT(10) NOT NULL,
+               exon_type VARCHAR(10),
+               gene_size INT(10) NOT NULL,
+               nb_intron_gene INT NOT NULL,
+               median_intron_size INT,
+               iupac_gene VARCHAR(50) NOT NULL,
+               upstream_exon_size INT,
+               exon_size INT,
+               downstream_exon_size INT,
+               upstream_intron_size INT,
+               downstream_intron_size INT,
+               force_acceptor_upstream_exon FLOAT,
+               force_acceptor FLOAT,
+               force_acceptor_downstream_exon FLOAT,
+               force_donor_upstream_exon FLOAT,
+               force_donor FLOAT,
+               force_donor_downstream_exon FLOAT,
+               iupac_exon VARCHAR(50),
+               iupac_upstream_intron_dist VARCHAR(50),
+               iupac_upstream_intron_proxi VARCHAR(50),
+               iupac_downstream_intron_proxi VARCHAR(50),
+               iupac_downstream_intron_dist VARCHAR(50),
+               PRIMARY KEY(gene_id, exon_pos));
+               """
+    cursor.execute(query)
+    sed_cnx.commit()
+
+
+def sed_filler(sed_cnx, list_tuple):
+    """
+    Fill the **sed database**
+
+    :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
+    SED database.
+    :param list_tuple: (list of **list info exon**). **list info exon** contains every information \
+    necessary for an exon.
+    """
+    cursor = sed_cnx.cursor()
+    cursor.executemany(
+        "INSERT INTO sed VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        list_tuple)
+    sed_cnx.commit()
+
 
 def main():
+    # debug mode
+    debug = 0  # 1 = enabled , 0 disabled
+
     cnx = fasterdbl_connection()
     info_list = exon_finder(cnx)
     exon_list = get_exon_info(cnx, info_list, debug)
     list_tuple = get_exon_tuple(exon_list)
-    write_result(list_tuple)
+    sed_cnx = sed_connection()
+    create_sed_exon_table(sed_cnx)
+    sed_filler(sed_cnx, list_tuple)
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()

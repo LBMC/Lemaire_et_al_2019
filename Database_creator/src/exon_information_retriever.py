1aa29b2b617bb10dac4c9e3c67cfda7d81140ba0
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-05-29 10:05:26 +0200

    src/exon_information_retriever.py - ADDITION OF 8 NEW FUNCTION AND MODIFICATION OF 4 FUNCTIONS.
    Those modification allow the creation of 2 more table in sed database : one table ase_events and one table rnaseq_project.
    The goal here is to easily retrieve the exon up or down regulated by some splicing factors

--- Database_creator/src/exon_information_retriever.py
+++ Database_creator/src/exon_information_retriever.py
@@ -8,13 +8,26 @@ import exon_class
 import sqlite3
 from database_creator import base_name, out_path
 import sys
+import re
+import conf
+import pymysql
 
 # functions
-def fasterdbl_connection():
+def connection_sl():
     """
+    :return: (pymysql object to connected to the splicing lore database)
+    """
+    cnx = pymysql.connect(user=conf.user, password=conf.password, host=conf.sl_host, database=conf.fasterDB)
+    return cnx
+
+
+def fasterdbl_connection(fasterdb):
+    """
+    :param fasterdb: (string) path to fasterDB database
+
     Allow connection to fasterDB Lite
     """
-    return sqlite3.connect(out_path + base_name)
+    return sqlite3.connect(fasterdb)
 
 
 def exon_finder(cnx):
@@ -82,11 +95,13 @@ def get_exon_tuple(exon_list):
     return list_tuple
 
 
-def sed_connection():
+def sed_connection(seddb):
     """
+    :param seddb: (string) path to sed database
+
     Connection to SED database
     """
-    return sqlite3.connect(out_path + "sed.db")
+    return sqlite3.connect(seddb)
 
 
 def create_sed_exon_table(sed_cnx):
@@ -126,9 +141,92 @@ def create_sed_exon_table(sed_cnx):
     sed_cnx.commit()
 
 
+def creation_rnaseq_projects_table(sed_cnx):
+    """
+    Create a rnaseq_projects table in ``sed_cnx``
+
+    :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
+    SED database.
+    """
+    cursor = sed_cnx.cursor()
+    query = """
+    CREATE TABLE rnaseq_projects (
+        id INT,
+        project_name VARCHAR(50) NOT NULL,
+        source_db VARCHAR(10) NOT NULL,
+        db_id_project VARCHAR(20) NOT NULL,
+        sf_name VARCHAR(25) NOT NULL,
+        cl_name VARCHAR(25) NOT NULL,
+        PRIMARY KEY (id)
+    );
+    """
+    cursor.execute(query)
+    sed_cnx.commit()
+
+
+def creation_ase_event_tmp_table(sed_cnx):
+    """
+    Create an ase_event_tmp table in ``sed_cnx``
+
+    :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
+    SED database.
+    """
+    cursor = sed_cnx.cursor()
+    query = """
+    CREATE TABLE ase_event_tmp (
+        id INT,
+        id_project INT NOT NULL,
+        gene_symbol VARCHAR(20) NOT NULL,
+        exon_skipped INT NOT NULL,
+        chromosome VARCHAR(1) NOT NULL,
+        start INT NOT NULL,
+        stop INT NOT NULL,
+        exons_flanquants VARCHAR(15),
+        delta_psi FLOAT,
+        pvalue FLOAT,
+        pvalue_glm_cor FLOAT,
+        PRIMARY KEY (id)
+    );
+    """
+    cursor.execute(query)
+    sed_cnx.commit()
+
+
+def creation_ase_event_table(sed_cnx):
+    """
+    Create an ase_event table in ``sed_cnx``
+
+    :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
+    SED database.
+    """
+    cursor = sed_cnx.cursor()
+    query = """
+    CREATE TABLE ase_event (
+        id INT,
+        id_project INT NOT NULL,
+        id_gene INT NOT NULL,
+        gene_symbol VARCHAR(20) NOT NULL,
+        exon_skipped INT NOT NULL,
+        chromosome VARCHAR(1) NOT NULL,
+        start INT NOT NULL,
+        stop INT NOT NULL,
+        exons_flanquants VARCHAR(15),
+        delta_psi FLOAT,
+        pvalue FLOAT,
+        pvalue_glm_cor FLOAT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (id_project) REFERENCES rnaseq_projects(id),
+        FOREIGN KEY (id_gene) REFERENCES sed(id_gene),
+        FOREIGN KEY (exon_skipped) REFERENCES sed(exon_pos)
+    );
+    """
+    cursor.execute(query)
+    sed_cnx.commit()
+
+
 def sed_filler(sed_cnx, list_tuple):
     """
-    Fill the **sed database**
+    Fill the **sed table**
 
     :param sed_cnx: (sqlite3 object) contains every information we need to connect to \
     SED database.
@@ -143,22 +241,141 @@ def sed_filler(sed_cnx, list_tuple):
     # creation of an index on gene_symbol and exon_pos
     query = """ CREATE INDEX sed_index on sed(gene_symbol, exon_pos);"""
     cursor.execute(query)
-    cursor.commit()
+    sed_cnx.commit()
+
+
+def fill_rnaseq_projects_content(cnx, sed_cnx):
+    """
+    Fill the table **rnaseq_projects** in ``sed database``.
+
+    :param cnx: (pymysql object) connection to splicing lore
+    :param sed_cnx: (sqlite3 object) connection to ``sed database``
+    """
+    cursor = cnx.cursor()
+    query = """
+    SELECT t1.id, t1.project_name, t1.source_db, t1.db_id_project, t2.name, t3.name_CL
+    FROM rnaseq_projects_SF t1, Splicing_factors t2, Cell_line t3
+    WHERE t1.id_SF = t2.id_SF
+    AND t1.id_cell_line = t3.id_cell_line
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor = sed_cnx.cursor()
+    cursor.executemany("INSERT INTO rnaseq_projects VALUES (?, ?, ?, ?, ?, ?)", result)
+    sed_cnx.commit()
+
+
+def fill_ase_event_tmp_content(cnx, sed_cnx):
+    """
+    Fill the table **ase_event_tmp** in ``sed database``.
+
+    :param cnx: (pymysql object) connection to splicing lore
+    :param sed_cnx: (sqlite3 object) connection to ``sed database``
+    """
+    cursor = cnx.cursor()
+    # selection of ASE events show in splicing Lore.
+    query = """
+    SELECT t1.id, t1.id_project, t1.gene_symbol, t1.exon_skipped, t1.coordonnees,
+    t1.exons_flaquants, t1.DeltaPSI, t1.pvalue, t1.pvalue_glm_corr
+    FROM Events_ASE t1, rnaseq_projects_SF t2
+    WHERE t1.id_project = t2.id
+    AND t2.show_in_website = 1
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    new_result = []
+    for row in result:
+        new_row = [row[i] for i in range(4)]
+        coord = re.split(":|-", row[4])
+        new_row.append(coord[0])
+        new_row.append(int(coord[1]))
+        new_row.append(int(coord[2]))
+        new_row += [row[i] for i in range(5, len(row), 1)]
+        new_result.append(new_row)
+
+    cursor = sed_cnx.cursor()
+    my_tuple = str(tuple((["?"] * len(new_result[0])))).replace("'", "")
+    cursor.executemany("INSERT INTO ase_event_tmp VALUES %s" % my_tuple, new_result)
+    sed_cnx.commit()
+
+
+def fill_ase_event_content(sed_cnx, seddb, fasterdblite):
+    """
+    Fill the table **ase_event** in ``sed_cnx``.
+
+    :param sed_cnx: (sqlite3 object) connection to ``sed database``
+    :param seddb: (string) path to sed database
+    :param fasterdblite: (string) path to fasterdblite database
+    """
+    query = """
+    SELECT t1.id, t1.id_project, t2.id_gene, t1.gene_symbol,
+    t1.exon_skipped, t1.chromosome, t1.start, t1.stop, t1.exons_flanquants, t1.delta_psi, t1.pvalue, t1.pvalue_glm_cor
+    FROM sed.ase_event_tmp t1, fasterdb.exons t2, fasterdb.genes t3
+    WHERE t2.id_gene = t3.id
+    AND t1.chromosome = t2.chromosome
+    AND t1.start = t2.start_on_chromosome
+    AND t1.stop = t2.end_on_chromosome
+    AND t3.official_symbol = t1.gene_symbol
+    AND t1.exon_skipped = t2.pos_on_gene
+    """
+
+    cursor = sed_cnx.cursor()
+    cursor.execute("ATTACH DATABASE ? as sed", (seddb,))
+    cursor.execute("ATTACH DATABASE ? as fasterdb", (fasterdblite,))
+    cursor.execute(query)
+    result = cursor.fetchall()
+    my_tuple = str(tuple((["?"] * len(result[0])))).replace("'", "")
+    cursor.executemany("INSERT INTO ase_event VALUES %s" % my_tuple, result)
+    query = """ CREATE INDEX sed_project on ase_event(id_project);"""
+    cursor.execute(query)
+    sed_cnx.commit()
+
+
+def remove_ase_event_tmp(sed_cnx):
+    """
+    Destroy the tables **ase_event_tmp** table.
+
+    :param sed_cnx: (sqlite3 object) connection to ``sed database``
+    """
+    cursor = sed_cnx.cursor()
+    cursor.execute("DROP TABLE ase_event_tmp;")
+    sed_cnx.commit()
 
 
 def main():
     # debug mode
     debug = 0  # 1 = enabled , 0 disabled
-
-    cnx = fasterdbl_connection()
+    fasterdblite = out_path + base_name
+    seddb = out_path + "sed.db"
+    cnx = fasterdbl_connection(out_path + base_name)
     info_list = exon_finder(cnx)
     exon_list = get_exon_info(cnx, info_list, debug)
     list_tuple = get_exon_tuple(exon_list)
-    sed_cnx = sed_connection()
+    cnx.close()
+    cnx = connection_sl()
+    sed_cnx = sed_connection(seddb)
+    print("Creation of SED table")
     create_sed_exon_table(sed_cnx)
+    print("Creation of rnaseq_projects table")
+    creation_rnaseq_projects_table(sed_cnx)
+    print("Creation of ase_event_tmp table")
+    creation_ase_event_tmp_table(sed_cnx)
+    print("Creation of ase_event table")
+    creation_ase_event_table(sed_cnx)
+    print("Filling sed table")
     sed_filler(sed_cnx, list_tuple)
+    print("Filling rnaseq_projects table")
+    fill_rnaseq_projects_content(cnx, sed_cnx)
+    print("Filling ase_event_tmp table")
+    fill_ase_event_tmp_content(cnx, sed_cnx)
+    print("Filling ase_event table")
+    fill_ase_event_content(sed_cnx, seddb, fasterdblite)
+    print("Removing ase_event_tmp table")
+    remove_ase_event_tmp(sed_cnx)
+    print("closing connections")
     cnx.close()
     sed_cnx.close()
+    print("successfully ended ! ")
 
 
 if __name__ == "__main__":

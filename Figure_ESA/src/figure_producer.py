f46c06b21a930442a98b1a203a25b14ec6f08cac
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-05-29 17:05:45 +0200

    src/figure_producer.py - initial commit

---
+++ Figure_ESA/src/figure_producer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.5
+
+# coding : utf8
+
+"""
+Description:
+
+    This script will display for each of up or down exon in a particular project every on of it's characteristics?
+    This will use the sed database
+"""
+
+
+# import
+import sqlite3

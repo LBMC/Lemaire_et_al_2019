6010a77baccf6b4826ebca97c67f5bacc20307eb
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-05-29 18:05:49 +0200

    src/figure_producer.py - production of get_list_of_value

--- Figure_ESA/src/figure_producer.py
+++ Figure_ESA/src/figure_producer.py
@@ -62,3 +62,24 @@ def get_ase_events(cnx, id_project, regulation):
     cursor.execute(query)
     return cursor.fetchall()
 
+
+def get_list_of_value(cnx, exon_list, target_column):
+    """
+    Get the individual values for ``target_column`` of every exon in ``exon_list``.
+
+    :param cnx: (sqlite3 connection object) connexion to sed database
+    :param exon_list: (list of tuple of 2 int) each sublist corresponds to an exon (gene_id + exon_position on gene)
+    :param target_column: (string) the column for which we want to get information on exons.
+    :return: (list of float) values of ``target_column`` for the exons in  ``exon_list``.
+    """
+    cursor = cnx.cursor()
+    res = []
+    for exon in exon_list:
+        query = """SELECT %s
+                   FROM sed
+                   where gene_id = %s
+                   AND exon_pos = %s """ % (target_column, exon[0], exon[1])
+        cursor.execute(query)
+        res.append(cursor.fetchone()[0])
+    return res
+

dcd7377b1dffaebb33c34ea55b440d1047da9ed2
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-05-29 17:16:30 +0200

    src/figure_producer.py - creation of connexion and get_interest_project function

--- Figure_ESA/src/figure_producer.py
+++ Figure_ESA/src/figure_producer.py
@@ -12,3 +12,31 @@ Description:
 
 # import
 import sqlite3
+
+
+# Functions
+def connexion(seddb):
+    """
+    Connexion to SED database.
+
+    :param seddb: ((string) path to sed database
+    :return:  (sqlite3 connection object) allow connexion to sed database
+    """
+    return sqlite3.connect(seddb)
+
+
+def get_interest_project(cnx):
+    """
+    Get the id of every project defined in sed database (from splicing lore).
+
+    :param cnx: (sqlite3 connection object) connexion to sed database
+    :return: (list of int) list of id_project
+    """
+    cursor = cnx.cursor()
+    query = "SELECT id FROM rnaseq_projects"
+    cursor.execute(query)
+    res = cursor.fetchall()
+    id = [val[0] for val in res]
+    return id
+
+

e61113a001146171180a37c77e2ae0e607c96b60
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-06-21 14:11:52 +0200

    src/heatmap_creator.py : creation of dnt heatmaps

--- Figure_ESA/src/heatmap_creator.py
+++ Figure_ESA/src/heatmap_creator.py
@@ -13,7 +13,9 @@ import union_dataset_function
 import os
 import random
 import sys
+sch = __import__('scipy.cluster.hierarchy')
 nt_list = ["A", "C", "G", "T"]
+dnt_list = ["AA", "AC", "AG", "AT", "CA", "CC", "CG", "CT", "GA", "GC", "GG", "GT", "TA", "TC", "TG", "TT"]
 
 # functions
 def create_matrix(cnx, id_projects, names, target_columns, control_dic, regulations, union=None):
@@ -84,10 +86,54 @@ def create_matrix_iupac(cnx, id_projects, names, target_columns, control_dic, re
                 exon_list = union_dataset_function.get_every_events_4_a_sl(cnx, names[i], regulation)
             for j in range(len(target_columns)):
                 for nt in nt_list:
-                    values = np.array(figure_producer.get_list_of_value_iupac(cnx, exon_list, target_columns[j], nt))
+                    values = np.array(figure_producer.get_list_of_value_iupac_dnt(cnx, exon_list, target_columns[j], nt))
                     median_obs = np.median(values[~np.isnan(values)])
                     final_value = float(median_obs - control_dic[target_columns[j]][nt]) / \
                         control_dic[target_columns[j]][nt] * 100
+                    project_res.append(final_value)
+            projects_tab.append(project_res)
+    return projects_tab, project_names, new_targets
+
+
+def create_matrix_alldnt(cnx, id_projects, names, target_columns, control_dic, regulations, union=None):
+    """
+    Create a matrix of relative medians (toward control) for dnt characteristics of an exon set.
+
+    :param cnx: (sqlite3 connect object) connexion to sed database
+    :param id_projects: (list of ints) the list of splicing lore project id.
+    :param names: (list of strings) the list of projects name (corresponding - in the same order - of the projects \
+    in ``id_projects``) or if union is not none: list of sf_name.
+    :param target_columns: (list of strings) list of interest characteristics for a set of exons.
+    :param control_dic: (dictionary of float) dictionary storing medians values for every characteristics of \
+    teh control set of exons.
+    :param regulations: (list of strings) the strings can be "up" or "down" only for up or down-regulated exons.
+    :param union: (None or string) None if we want to work project by project, anything else to work \
+    with exons regulation by a particular splicing factor.
+    :return: (lif of list of float) the medians value for a project (line) for every characteristic of \
+    interests (number of value in one line corresponding to a project).
+    """
+    new_targets = [target_columns[i].replace("dnt", "%s_dnt" % dnt)
+                   for i in range(len(target_columns)) for dnt in remove_dnt(control_dic, target_columns[i])]
+    project_names = []
+    projects_tab = []
+    for i in range(len(names)):
+        for regulation in regulations:
+            project_res = []
+            project_names.append("%s_%s" % (names[i], regulation))
+            if not union:
+                exon_list = figure_producer.get_ase_events(cnx, id_projects[i], regulation)
+            else:
+                exon_list = union_dataset_function.get_every_events_4_a_sl(cnx, names[i], regulation)
+            for j in range(len(target_columns)):
+                new_dnt_list = remove_dnt(control_dic, target_columns[j])
+                for dnt in new_dnt_list:
+                    values = np.array(figure_producer.get_list_of_value_iupac_dnt(cnx, exon_list, target_columns[j], dnt))
+                    median_obs = np.median(values[~np.isnan(values)])
+                    if control_dic[target_columns[j]][dnt] != 0:
+                        final_value = float(median_obs - control_dic[target_columns[j]][dnt]) / \
+                                      control_dic[target_columns[j]][dnt] * 100
+                    else:
+                        final_value = float("nan")
                     project_res.append(final_value)
             projects_tab.append(project_res)
     return projects_tab, project_names, new_targets
@@ -122,9 +168,49 @@ def create_matrix_nt(cnx, id_projects, names, target_column, control_dic, nt, re
                 exon_list = figure_producer.get_ase_events(cnx, id_projects[i], regulation)
             else:
                 exon_list = union_dataset_function.get_every_events_4_a_sl(cnx, names[i], regulation)
-            values = np.array(figure_producer.get_list_of_value_iupac(cnx, exon_list, target_column, nt))
+            values = np.array(figure_producer.get_list_of_value_iupac_dnt(cnx, exon_list, target_column, nt))
             median_obs = np.median(values[~np.isnan(values)])
             final_value = float(median_obs - control_dic[target_column][nt]) / control_dic[target_column][nt] * 100
+            project_res.append(final_value)
+            projects_tab.append(project_res)
+    return projects_tab, project_names, new_targets
+
+
+def create_matrix_dnt(cnx, id_projects, names, target_column, control_dic, dnt, regulations, union=None):
+    """
+    Create a matrix of relative medians (toward control) for a single dnt of an exon set.
+
+    :param cnx: (sqlite3 connect object) connexion to sed database
+    :param id_projects: (list of ints) the list of splicing lore project id.
+    :param names: (list of strings) the list of projects name (corresponding - in the same order - of the projects \
+    in ``id_projects``) or if union is not none: list of sf_name.
+    :param target_column: (string) characteristic of interest for a set of exons.
+    :param control_dic: (dictionary of float) dictionary storing medians values for every characteristics of \
+    teh control set of exons.
+    :param dnt: (string) a di-nucleotides.
+    :param regulations: (list of strings) the strings can be "up" or "down" only for up or down-regulated exons.
+    :param union: (None or string) None if we want to work project by project, anything else to work \
+    with exons regulation by a particular splicing factor.
+    :return: (lif of list of float) the medians value for a project (line) for the characteristic of \
+    interest on one nucleotide (number of value in one line corresponding to a project).
+    """
+    new_targets = target_column.replace("dnt", "%s_dnt" % dnt)
+    project_names = []
+    projects_tab = []
+    for i in range(len(names)):
+        for regulation in regulations:
+            project_res = []
+            project_names.append("%s_%s" % (names[i], regulation))
+            if not union:
+                exon_list = figure_producer.get_ase_events(cnx, id_projects[i], regulation)
+            else:
+                exon_list = union_dataset_function.get_every_events_4_a_sl(cnx, names[i], regulation)
+            values = np.array(figure_producer.get_list_of_value_iupac_dnt(cnx, exon_list, target_column, dnt))
+            median_obs = np.median(values[~np.isnan(values)])
+            if control_dic[target_column][dnt] != 0:
+                final_value = float(median_obs - control_dic[target_column][dnt]) / control_dic[target_column][dnt] * 100
+            else:
+                final_value = float("nan")
             project_res.append(final_value)
             projects_tab.append(project_res)
     return projects_tab, project_names, new_targets
@@ -148,7 +234,7 @@ def simple_heatmap(data_array, labelsy, labelsx, output, name=""):
     d = {labelsy[i]: {labelsx[j]: [i, j] for j in range(len(labelsx))} for i in range(len(labelsy))}
     # Initialize figure by creating upper dendrogram
     # Create Side Dendrogram
-    figure = ff.create_dendrogram(data_array, orientation='right', labels=labelsy)
+    figure = ff.create_dendrogram(data_array, orientation='right', labels=labelsy, linkagefun=lambda x: sch.cluster.hierarchy.linkage(x, 'complete'))
     for i in range(len(figure['data'])):
         figure['data'][i]['xaxis'] = 'x2'
 
@@ -233,12 +319,12 @@ def heatmap_creator(data_array, labelsx, labelsy, output, name=""):
     d = {labelsy[i]: {labelsx[j]: [i, j] for j in range(len(labelsx))} for i in range(len(labelsy))}
     # Initialize figure by creating upper dendrogram
     data_side = data_array.transpose()
-    figure = ff.create_dendrogram(data_side, orientation='bottom', labels=labelsx)
+    figure = ff.create_dendrogram(data_side, orientation='bottom', labels=labelsx, linkagefun=lambda x: sch.cluster.hierarchy.linkage(x, 'complete'))
     for i in range(len(figure['data'])):
         figure['data'][i]['yaxis'] = 'y2'
 
     # Create Side Dendrogram
-    dendro_side = ff.create_dendrogram(data_array, orientation='right', labels=labelsy)
+    dendro_side = ff.create_dendrogram(data_array, orientation='right', labels=labelsy, linkagefun=lambda x: sch.cluster.hierarchy.linkage(x, 'complete'))
     for i in range(len(dendro_side['data'])):
         dendro_side['data'][i]['xaxis'] = 'x2'
 
@@ -315,6 +401,23 @@ def heatmap_creator(data_array, labelsx, labelsy, output, name=""):
                         auto_open=False)
 
 
+def remove_dnt(control_dic, target_column):
+    """
+    Remove the dnt where control frequencies equals to 0.
+
+    :param control_dic: (dictionary of float) dictionary storing medians values for every characteristics of \
+    teh control set of exons.
+    :param target_column: (string) column_of_interest
+    :return: (list of string) list of dnt of interest
+    """
+    new_dnt_list = []
+    for dnt in dnt_list:
+        if control_dic[target_column][dnt] != 0:
+            new_dnt_list.append(dnt)
+    return new_dnt_list
+
+
+
 def main():
     """
     Launch the main function.
@@ -324,14 +427,15 @@ def main():
     cnx = figure_producer.connexion(seddb)
     ctrl_dic = exon_control_handler.control_handler(cnx, exon_type)
     if len(sys.argv) < 2:
-        output = "/".join(os.path.realpath(__file__).split("/")[:-2]) + "/result/heatmap_test/"
+        output = "/".join(os.path.realpath(__file__).split("/")[:-2]) + "/result/heatmap/"
         # If the output directory does not exist, then we create it !
         if not os.path.isdir(output):
             os.mkdir(output)
         id_projects, name_projects = figure_producer.get_interest_project(cnx)
         for regulations in [["up"], ["down"], ["up", "down"]]:
             # Creating heatmap
-            target_columns = ["gene_size", "nb_intron_gene", "median_intron_size", "upstream_exon_size", "exon_size", "downstream_exon_size", "upstream_intron_size", "downstream_intron_size"]
+            target_columns = ["gene_size", "nb_intron_gene", "median_intron_size", "upstream_exon_size", "exon_size",
+                              "downstream_exon_size", "upstream_intron_size", "downstream_intron_size"]
             projects_tab, project_names = create_matrix(cnx, id_projects, name_projects, target_columns, ctrl_dic,
                                                         regulations)
             heatmap_creator(np.array(projects_tab[0]), target_columns, project_names, output,
@@ -358,7 +462,29 @@ def main():
                     projects_tab, project_names, new_target = create_matrix_nt(cnx, id_projects, name_projects,
                                                                                target_column, ctrl_dic, nt, regulations)
                     simple_heatmap(projects_tab, project_names, new_target, output, "_".join(regulations))
-    if sys.argv[1] == "union":
+
+            # handling dnt heatmap
+            #  All dnt at once
+            target_columns = ["dnt_exon", "dnt_gene", "dnt_upstream_intron_dist", "dnt_upstream_intron_proxi",
+                              "dnt_downstream_intron_proxi", "dnt_downstream_intron_dist"]
+            projects_tab, project_names, new_targets = create_matrix_alldnt(cnx, id_projects, name_projects,
+                                                                            target_columns, ctrl_dic, regulations)
+            heatmap_creator(np.array(projects_tab), new_targets, project_names, output,
+                            "all_dnt_" + "_".join(regulations))
+            # dnt by dnt
+            for target_column in target_columns:
+                projects_tab, project_names, new_targets = create_matrix_alldnt(cnx, id_projects, name_projects,
+                                                                                [target_column], ctrl_dic, regulations)
+                heatmap_creator(np.array(projects_tab), new_targets, project_names, output,
+                                target_column + "_" + "_".join(regulations))
+                new_dnt_list = remove_dnt(ctrl_dic, target_column)
+                for dnt in new_dnt_list:
+                    projects_tab, project_names, new_target = create_matrix_dnt(cnx, id_projects, name_projects,
+                                                                                target_column, ctrl_dic, dnt,
+                                                                                regulations)
+                    simple_heatmap(projects_tab, project_names, new_target, output, "_".join(regulations))
+
+    elif sys.argv[1] == "union":
         output = "/".join(os.path.realpath(__file__).split("/")[:-2]) + "/result/heatmap_union/"
         # If the output directory does not exist, then we create it !
         if not os.path.isdir(output):
@@ -398,6 +524,28 @@ def main():
                                                                                "union")
                     simple_heatmap(projects_tab, project_names, new_target, output, "_".join(regulations))
 
+            # handling dnt heatmap
+            #  All dnt at once
+            target_columns = ["dnt_exon", "dnt_gene", "dnt_upstream_intron_dist", "dnt_upstream_intron_proxi",
+                              "dnt_downstream_intron_proxi", "dnt_downstream_intron_dist"]
+            projects_tab, project_names, new_targets = create_matrix_alldnt(cnx, None, name_projects,
+                                                                            target_columns, ctrl_dic, regulations,
+                                                                            "union")
+            heatmap_creator(np.array(projects_tab), new_targets, project_names, output,
+                            "all_dnt_" + "_".join(regulations))
+            # dnt by dnt
+            for target_column in target_columns:
+                projects_tab, project_names, new_targets = create_matrix_alldnt(cnx, None, name_projects,
+                                                                                [target_column], ctrl_dic, regulations,
+                                                                                "union")
+                heatmap_creator(np.array(projects_tab), new_targets, project_names, output,
+                                target_column + "_" + "_".join(regulations))
+                new_dnt_list = remove_dnt(ctrl_dic, target_column)
+                for dnt in new_dnt_list:
+                    projects_tab, project_names, new_target = create_matrix_dnt(cnx, None, name_projects,
+                                                                                target_column, ctrl_dic, dnt,
+                                                                                regulations, "union")
+                    simple_heatmap(projects_tab, project_names, new_target, output, "_".join(regulations))
 
 if __name__ == "__main__":
     main()

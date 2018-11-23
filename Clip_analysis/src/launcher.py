3620f396f07c26c8b8e1d851b9522fadd59596ae
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-11-23 12:56:03 +0100

    src/launcher.py : modification of the main function (no more creation of crtl set and result figure) but only a creation of the nucleotide frequencies file + modif to match pep8

--- Clip_analysis/src/launcher.py
+++ Clip_analysis/src/launcher.py
@@ -3,70 +3,54 @@
 # -*- coding: utf-8 -*-
 
 import get_bed_sequences
-import figure_maker
 import bed_handler
 import argparse
 import os
-import create_control_sets2 as create_control_sets
-
-#import create_control_sets
 
 
-def main(clip_bed, fasterdb_bed, hg19_reference, output, size, prg_path, enrichment, nb_iteration, exon_type,
-         hg19_chromosome_file):
+def main(clip_bed, bed_folder, hg19_reference, output, size, prg_path, enrichment):
     """
-    Create a weblogo of the sequences in ``clip_bed`` overlapping those in ``fasterdb_bed``
+    Create a weblogo of the sequences in ``clip_bed`` overlapping those in ``fasterdb_bed``.
+
     :param clip_bed: (string) a bed containing motifs bing by a particular splicing factor
-    :param fasterdb_bed: (string) a bed containing every fasterDB exons with 200bp of interval
+    :param bed_folder: (string)a folder containing interest bed files
     :param hg19_reference: (string) a fasta file containing the sequences of every chromosome in hg19
     :param output: (string) path where the weblogo will be created
     :param size: (int) the size of the weblogo
     :param prg_path: (string) path to motif finder programs
     :param enrichment: (string) y/n : y to perform en enrichment motif analysis and N to \
     produce only a weblogo centered in the clip sequence
-    :param nb_iteration: (int) the chosen number of iteration to generated the control sequence
-    :param exon_type: (string) the control exon chosen (CCE, ACE or ALL)
-    :param hg19_chromosome_file: (string) a file containing hg19 chromosome size
     """
-    project_name = os.path.basename(clip_bed).split(".")[0]
-    outputs =[output + "1based_bed/", output + "intesect_bed/", output + "figures/"]
+    outputs = [output + "1based_bed/", output + "intesect_bed/", output + "figures/"]
     for cur_output in outputs:
         if not os.path.isdir(cur_output):
             os.mkdir(cur_output)
     print("Creation of a one based bed")
-    b1_bed = bed_handler.bed_0based_2_1based(clip_bed, outputs[0])
-    # print("intersection with fasterDB bed")
-    # intersect_bed = bed_handler.intersect_bed(fasterdb_bed, b1_bed, outputs[1])
+    bed_list = [bed_handler.bed_0based_2_1based(clip_bed, outputs[0])]
+    print("Creation of intersect bed")
+    list_template = bed_handler.get_files(bed_folder)
+    for bed in list_template:
+        bed_list.append(bed_handler.intersect_bed(bed, bed_list[0], outputs[1]))
     print("Loading hg19 genome !")
     dic_records = get_bed_sequences.create_sequence_dic(hg19_reference)
-    # print("getting weblogo sequences")
-    # list_coordinates = get_bed_sequences.get_bed_coordinates(intersect_bed)
-    list_coordinates = get_bed_sequences.get_bed_coordinates(b1_bed, dic_records.keys())
-    print("nb list_coordinates")
-    print(len(list_coordinates))
-    # list_sequence = get_bed_sequences.get_middle_sequences(list_coordinates, dic_records, size)
-    # print("Making weblogo sequence")
-    # figure_maker.web_logo_creator(list_sequence, project_name, outputs[2])
-    print("Creating control exons sets")
-    create_control_sets.set_debug(0)
-    mean_ctrl_dic = create_control_sets.make_control_sets(nb_iteration, hg19_chromosome_file, fasterdb_bed, exon_type,
-                                          list_coordinates, dic_records)
-    print("creating test_set")
-    # print("coord test : %s" % str(list_coordinates[0:3]))
-    # mean_test_dic = create_control_sets.create_mean_frequency_dic(create_control_sets.get_sequences_frequencies(list_coordinates[0:3], dic_records))
-    mean_test_dic = create_control_sets.create_mean_frequency_dic(
-        create_control_sets.get_sequences_frequencies_test(list_coordinates, dic_records))
-    figure_maker.make_control_figures(mean_test_dic, mean_ctrl_dic, outputs[2], exon_type, project_name, nb_iteration)
-
-    if enrichment == "Y":
-        motif_search_dir = output + "motif_search/"
-        if not os.path.isdir(motif_search_dir):
-            os.mkdir(motif_search_dir)
-        fa_file = get_bed_sequences.write_fasta_file(list_coordinates, dic_records, motif_search_dir)
-        del (dic_records)
-        print("Searching enriched motifs")
-        bed_handler.meme_launcher(prg_path, fa_file, motif_search_dir)
-        bed_handler.zagros_launcher(prg_path, fa_file, size, motif_search_dir)
+    for i in range(len(bed_list)):
+        print("Working on %s bed file" % bed_list[i])
+        list_coordinates = get_bed_sequences.get_bed_coordinates(bed_list[i], dic_records.keys())
+        print("   --> nb list_coordinates: %s" % len(list_coordinates))
+        list_sequence = get_bed_sequences.get_middle_sequences(list_coordinates, dic_records, size)
+        dic_freq = get_bed_sequences.get_nt_frequencies(list_sequence)
+        name_result = os.path.basename(bed_list[i]).split(".")[0]
+        with open("%sfrequencies_%s.py" % (output, name_result), "w") as outfile:
+            outfile.write("dic_freq = %s\n" % str(dic_freq))
+        if enrichment == "Y":
+            motif_search_dir = output + "motif_search_%s/" % name_result
+            if not os.path.isdir(motif_search_dir):
+                os.mkdir(motif_search_dir)
+            fa_file = get_bed_sequences.write_fasta_file(list_coordinates, dic_records, motif_search_dir)
+            print("Searching enriched motifs")
+            bed_handler.meme_launcher(prg_path, fa_file, motif_search_dir)
+            zagros_size = min(size, 8)
+            bed_handler.zagros_launcher(prg_path, fa_file, zagros_size, motif_search_dir)
 
 
 def launcher():
@@ -86,24 +70,25 @@ def launcher():
 
     req_arg.add_argument('--clip_bed', dest='clip_bed', help="bed coming from a clip experiment",
                          required=True)
-    parser.add_argument('--enrichment', dest='enrichment', help="(Y/N)Y perform a motif search in clipped sequence (time consuming), "
-                                                                 "N if you want only a belogo centered in the clip sequence",
-                        default="N")
+    parser.add_argument('--enrichment', dest='enrichment',
+                        help="(Y/N)Y perform a motif search in clipped sequence (time consuming), "
+                             "N if you want only a belogo centered in the clip sequence", default="N")
     parser.add_argument('--fasterdb_bed', dest='fasterdb_bed', help="bed file containing the fasterdb exons with "
                                                                     "200 additional bp at each side of the exon.",
-                        default=os.path.realpath(os.path.dirname(__file__)).replace("src",
-                                                                                     "data/fasterDB_exons_add200nt.bed"))
+                        default=os.path.realpath(os.path.dirname(__file__)).
+                        replace("src", "data/fasterDB_exons_add200nt.bed"))
     parser.add_argument('--hg19_reference', dest='hg19_reference',
                         help="fasta file containing the sequences of every chromosome in hg19",
                         default=os.path.realpath(
-                            os.path.dirname(__file__)).replace("src","data/Homo_sapiens.GRCh37.dna.primary_assembly.fa"))
+                            os.path.dirname(__file__)).replace("src",
+                                                               "data/Homo_sapiens.GRCh37.dna.primary_assembly.fa"))
     req_arg.add_argument('--output', dest='output', help="path where the weblogo will be created",
-                        required=True)
-    parser.add_argument('--size', dest='size', help='weblogo size', default=6)
+                         required=True)
+    parser.add_argument('--size', dest='size', help='weblogo size', default=20)
     parser.add_argument('--nb_iteration', dest='nb_iteration', help='nb control iteration', default=1000)
     parser.add_argument('--exon_type', dest='exon_type', help='exon type (ACE, CCE, or ALL)', default="CCE")
     parser.add_argument('--meme_path', dest="meme_path", help="path to meme program",
-                        default=os.path.realpath(os.path.dirname(__file__)).replace("src","data/meme_program/"))
+                        default=os.path.realpath(os.path.dirname(__file__)).replace("src", "data/meme_program/"))
     parser.add_argument('--chr_size', dest="chr_size", help="a tab file indicating the size of each chromosome in hg19",
                         default=os.path.realpath(os.path.dirname(__file__)).replace("src", "data/hg19.ren.chrom.sizes"))
 
@@ -124,10 +109,10 @@ def launcher():
         args.size = int(args.size)
         if args.size < 0:
             parser.error("ERROR : wrong weblogo size")
-    except:
+    except ValueError:
         parser.error("ERROR : wrong weblogo size")
 
-    response = {"yes": "Y", "y": "Y", "Y":"Y", "no": "N", "n": "N", "N": "N"}
+    response = {"yes": "Y", "y": "Y", "Y": "Y", "no": "N", "n": "N", "N": "N"}
     if args.enrichment not in response.keys():
         parser.error("Wrong value for --enrichment, the values Y/N/y/n/yes/no are accepted")
     else:
@@ -140,10 +125,8 @@ def launcher():
     except ValueError:
         parser.error("Wrong parameter for nb_iteration, it must be a number")
 
-    main(args.clip_bed, args.fasterdb_bed, args.hg19_reference, args.output, args.size, args.meme_path, args.enrichment,
-         args.nb_iteration, args.exon_type, args.chr_size)
+    main(args.clip_bed, args.fasterdb_bed, args.hg19_reference, args.output, args.size, args.meme_path, args.enrichment)
 
 
 if __name__ == "__main__":
     launcher()
-

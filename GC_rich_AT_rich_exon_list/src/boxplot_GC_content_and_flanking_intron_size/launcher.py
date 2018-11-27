5e0bfb61af451e3c3c6517ec1e572bdff31f211c
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2018-11-27 15:45:02 +0100

    src/boxplot_GC_content_and_flanking_intron_size/ : stat + separate figure for gene and exon + only pure group

--- GC_rich_AT_rich_exon_list/src/boxplot_GC_content_and_flanking_intron_size/launcher.py
+++ GC_rich_AT_rich_exon_list/src/boxplot_GC_content_and_flanking_intron_size/launcher.py
@@ -12,9 +12,34 @@ import sqlite3
 import plotly.graph_objs as go
 import plotly
 import math
+import sys
+import rpy2.robjects as robj
+import rpy2.robjects.vectors as v
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)).replace("boxplot_GC_content_and_flanking_intron_size", ""))
+import group_factor
 
 
-def create_figure(list_values, list_name, output, regulation, name_fig):
+def mann_withney_test_r(list_values1, list_values2):
+    """
+    Perform a mann withney wilcoxon test on ``list_values1`` and ``list_values2``.
+
+    :param list_values1: (list of float)  list of float
+    :param list_values2: (list of float)  list of float
+    :return: (float) the pvalue of the mann withney test done one `list_values1`` and ``list_values2``.
+    """
+    wicox = robj.r("""
+
+    function(x, y){
+        test = wilcox.test(x,y, alternative='two.sided', correct=F)
+        return(test$p.value)
+    }
+
+                   """)
+    pval = float(wicox(v.FloatVector(list_values1), v.FloatVector(list_values2))[0])
+    return pval
+
+
+def create_figure(list_values, list_name, output, regulation, name_fig, type_fig):
     """
     Create a figure showing the values of ``target_name`` for every exon list regulated by ``list_name`` factor
 
@@ -24,20 +49,25 @@ def create_figure(list_values, list_name, output, regulation, name_fig):
     :param output: (string) path where the output_file will be created
     :param regulation: (string) up or down
     :param name_fig: (string) the name of the graphic
+    :param type_fig: (string) the type of graphic build
     """
-    color_list = ["#0000FF", "#0394d9", "#00aa00", "#03d994", "#FF0000"] * 2
+
+    color_dic = group_factor.color_dic
     data = []
-    title = """%s of %s exons and gene containing those exons regulated by different factors""" % (name_fig, regulation)
+    title = """%s of %s %s containing those exons regulated by different factors""" % (name_fig, regulation, type_fig)
     if "intron" in name_fig:
         list_values = [list(map(math.log10, cur_list)) for cur_list in list_values]
         title += "<br/>(median flanking intron size for exons and median introns size for genes)"
     if "gene_size" in name_fig:
         list_values = [list(map(math.log10, cur_list)) for cur_list in list_values]
         title = """%s of %s exons of different exons set""" % (name_fig, regulation)
+    for i in range(len(list_name) - 1):
+        pval = mann_withney_test_r(list_values[i], list_values[-1])
+        title += "<br> mw two-sided test %s vs %s : p = %.2E" % (list_name[i], list_name[-1], pval)
     for i in range(len(list_values)):
         data.append({"y": list_values[i], "type": "violin",
                      "name": list_name[i], "visible": True,  # "fillcolor": color_list[i], "opacity": 0.6,
-                     "line": {"color": color_list[i]},
+                     "line": {"color": color_dic["_".join(list_name[i].split("_")[:-1])]},
                      "box": {"visible": True}, "meanline": {"visible": True}})
 
     layout = go.Layout(
@@ -59,13 +89,13 @@ def create_figure(list_values, list_name, output, regulation, name_fig):
             b=150,
             t=100,
         ),
-        paper_bgcolor='rgb(243, 243, 243)',
-        plot_bgcolor='rgb(243, 243, 243)',
+        paper_bgcolor='white',
+        plot_bgcolor='white',
         showlegend=True
     )
 
     fig = {"data": data, "layout": layout}
-    plotly.offline.plot(fig, filename="%s%s_%s_boxplot.html" % (output, name_fig, regulation),
+    plotly.offline.plot(fig, filename="%s%s_%s_boxplot_%s.html" % (output, name_fig, regulation, type_fig),
                         auto_open=False, validate=False)
 
 
@@ -82,42 +112,42 @@ def main():
     cnx = sqlite3.connect(seddb)
     at_file_pure = "%sAT_rich_exons" % path
     gc_file_pure = "%sGC_rich_exons" % path
-    at_file_all = "%sAT_rich_with_intersection_exons" % path
-    gc_file_all = "%sGC_rich_with_intersection_exons" % path
-    name_file = ["GC_pure_exons", "GC_all_exons", "AT_pure_exons", "AT_all_exons", "%s_exons" % exon_type]
-    name_file = name_file + [res.replace("exons", "genes") for res in name_file]
-    list_file = [gc_file_pure, gc_file_all, at_file_pure, at_file_all, None] * 2
-    list_gc_content = []
-    list_intron_size = []
-    list_gene_size = []
-    for i in range(len(name_file)):
-        if "exons" in name_file[i]:
-            if exon_type not in name_file[i]:
-                list_gc_content.append(boxplot_gc_content_maker.extract_exon_gc_content_from_file(cnx, list_file[i]))
-                list_intron_size.append(
-                    boxplot_flanking_intron_size.extract_exon_median_flanking_intron_size_from_file(cnx, list_file[i])
-                )
-            else:
-                list_gc_content.append(boxplot_gc_content_maker.get_exon_control_gc_content(cnx, exon_type))
-                list_intron_size.append(
-                    boxplot_flanking_intron_size.get_exon_control_median_flanking_intron_size(cnx, exon_type)
-                )
-        if "genes" in name_file[i]:
-            if exon_type not in name_file[i]:
-                list_gc_content.append(boxplot_gc_content_maker.extract_gene_gc_content_from_file(cnx, list_file[i]))
-                list_intron_size.append(
-                    boxplot_flanking_intron_size.extract_gene_median_intron_size_from_file(cnx, list_file[i])
-                )
-                list_gene_size.append(boxplot_gene_size.extract_gene_size_from_file(cnx, list_file[i]))
-            else:
-                list_gc_content.append(boxplot_gc_content_maker.get_gene_control_gc_content(cnx, exon_type))
-                list_intron_size.append(
-                    boxplot_flanking_intron_size.get_gene_control_median_flanking_intron_size(cnx, exon_type)
-                )
-                list_gene_size.append(boxplot_gene_size.get_control_gene_size(cnx, exon_type))
-    create_figure(list_gc_content, name_file, output, "down", "GC_content")
-    create_figure(list_intron_size, name_file, output, "down", "median_intron_size")
-    create_figure(list_gene_size, name_file, output, "down", "gene_size")
+    levels = ["exons", "genes"]
+    for my_level in levels:
+        name_file = ["GC_pure_%s" % my_level, "AT_pure_%s" % my_level, "%s_%s" % (exon_type, my_level)]
+        list_file = [gc_file_pure, at_file_pure, None]
+        list_gc_content = []
+        list_intron_size = []
+        list_gene_size = []
+        for i in range(len(name_file)):
+            if "exons" in name_file[i]:
+                if exon_type not in name_file[i]:
+                    list_gc_content.append(boxplot_gc_content_maker.extract_exon_gc_content_from_file(cnx, list_file[i]))
+                    list_intron_size.append(
+                        boxplot_flanking_intron_size.extract_exon_median_flanking_intron_size_from_file(cnx, list_file[i])
+                    )
+                else:
+                    list_gc_content.append(boxplot_gc_content_maker.get_exon_control_gc_content(cnx, exon_type))
+                    list_intron_size.append(
+                        boxplot_flanking_intron_size.get_exon_control_median_flanking_intron_size(cnx, exon_type)
+                    )
+            if "genes" in name_file[i]:
+                if exon_type not in name_file[i]:
+                    list_gc_content.append(boxplot_gc_content_maker.extract_gene_gc_content_from_file(cnx, list_file[i]))
+                    list_intron_size.append(
+                        boxplot_flanking_intron_size.extract_gene_median_intron_size_from_file(cnx, list_file[i])
+                    )
+                    list_gene_size.append(boxplot_gene_size.extract_gene_size_from_file(cnx, list_file[i]))
+                else:
+                    list_gc_content.append(boxplot_gc_content_maker.get_gene_control_gc_content(cnx, exon_type))
+                    list_intron_size.append(
+                        boxplot_flanking_intron_size.get_gene_control_median_flanking_intron_size(cnx, exon_type)
+                    )
+                    list_gene_size.append(boxplot_gene_size.get_control_gene_size(cnx, exon_type))
+        create_figure(list_gc_content, name_file, output, "down", "GC_content", my_level)
+        create_figure(list_intron_size, name_file, output, "down", "median_intron_size", my_level)
+        if my_level == "genes":
+            create_figure(list_gene_size, name_file, output, "down", "gene_size", my_level)
 
 
 if __name__ == "__main__":

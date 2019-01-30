338903e0cb9214b57a7325b54ff68e53bd5c393b
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-01-30 16:30:58 +0100

    src/stretch_calculator/config.py :  config file to select the stretch wanted on the upstream intronic sequence wanted and on the nucleotides wanted

---
+++ GC_rich_AT_rich_exon_list/src/stretch_calculator/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Description:
+    This script contains parameters used for the streches program
+"""
+
+exon_type = ["CCE"]
+stretches = [[4, 3], [5, 4], [6, 5], [7, 6]]
+sequence_boundaries = [-74, -34]
+abscissa = ["<=2", ">=3"]
+#nt_list = ["A", "C", "G", "T", "S", "W", "R", "Y"]
+nt_list = ["T"]

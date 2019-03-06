db5442c3026e36b174d0777ef8a1086ea82d1ce3
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-03-06 13:18:18 +0100

    src/make_control_files_bp_ppt/stat_bp.py : a script to perform statistical analysis

---
+++ GC_rich_AT_rich_exon_list/src/make_control_files_bp_ppt/stat_bp.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+from rpy2.robjects import r, pandas2ri
+
+pandas2ri.activate()
+
+
+def glm_poisson_stats(dataframe, filename):
+    """
+    Perform a glm nb test on ``list_values1`` and ``list_values2``.
+
+    :param dataframe: (pandas DataFrame) a dataframe
+    :param filename: (string)  list of float
+    :return: (float) the pvalue of the mann withney test done one `list_values1`` and ``list_values2``.
+    """
+    dataframe = dataframe[dataframe["project"] != "CCE_exons"]
+    r_df = pandas2ri.py2ri(dataframe)
+    stat_s = r("""
+    require("DHARMa")
+    require("multcomp")
+    function(data, name){
+        factors <- levels(as.factor(as.vector(data$project)))
+        data$project <- as.factor(as.vector(data$project))
+        mlm <- glm(values ~ project, data=data, family="poisson")
+        simulationOutput <- simulateResiduals(fittedModel = mlm, n = 250)
+        png(paste(name, "_dignostics.png", sep=""), height=1080, width=1920)
+        par(mfrow=c(2, 2))
+        plot(simulationOutput)
+        dev.off()
+        s <- summary(glht(mlm, mcp(project = "Tukey")))
+        tab <- as.data.frame(cbind(names(s$test$coefficients), s$test$coefficients, s$test$sigma,  s$test$tstat, s$test$pvalues))
+        colnames(tab) <- c("Comparison", "Estimate", "Std.Error", "z.value", "Pr(>|z|)")
+        return(tab)
+    }
+               """)
+    name = filename.split(".")[0]
+    df_stats = pandas2ri.ri2py(stat_s(r_df, name))
+    return df_stats
+
+
+def glm_nb_stats(dataframe, filename):
+    """
+    Perform a glm nb test on ``list_values1`` and ``list_values2``.
+
+    :param dataframe: (pandas DataFrame) a dataframe
+    :param filename: (string)  list of float
+    :return: (float) the pvalue of the mann withney test done one `list_values1`` and ``list_values2``.
+    """
+    dataframe = dataframe[dataframe["project"] != "CCE_exons"]
+    r_df = pandas2ri.py2ri(dataframe)
+    stat_s = r("""
+    require("DHARMa")
+    require("multcomp")
+    function(data, name){
+        factors <- levels(as.factor(as.vector(data$project)))
+        data$project <- as.factor(as.vector(data$project))
+        mlm <- glm.nb(values ~ project, data=data)
+        simulationOutput <- simulateResiduals(fittedModel = mlm, n = 250)
+        png(paste(name, "_dignostics.png", sep=""), height=1080, width=1920)
+        par(mfrow=c(2, 2))
+        plot(simulationOutput)
+        dev.off()
+        s <- summary(glht(mlm, mcp(project = "Tukey")))
+        tab <- as.data.frame(cbind(names(s$test$coefficients), s$test$coefficients, s$test$sigma,  s$test$tstat, s$test$pvalues))
+        colnames(tab) <- c("Comparison", "Estimate", "Std.Error", "z.value", "Pr(>|z|)")
+        return(tab)
+    }
+               """)
+    name = filename.split(".")[0]
+    df_stats = pandas2ri.ri2py(stat_s(r_df, name))
+    return df_stats

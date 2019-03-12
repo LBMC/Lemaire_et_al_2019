d31e78874d27ebca1e742db0200d11325993ceec
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-03-12 10:04:45 +0100

    src/metaexon_figure/control_dictionnary.py : modification of get_control_exon_information functions to remove exons regulated by splicing factor from the control set

--- GC_rich_AT_rich_exon_list/src/metaexon_figure/control_dictionnary.py
+++ GC_rich_AT_rich_exon_list/src/metaexon_figure/control_dictionnary.py
@@ -7,14 +7,17 @@ import os
 import sqlite3
 import exon_class
 import win_size
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).replace("/metaexon_figure", ""))
+import union_dataset_function
 
-
-def get_control_exon_information(cnx, exon_type):
+def get_control_exon_information(cnx, exon_type, exon2remove):
     """
     Get the gene symbol, the gene id and the position of every ``exon_type`` exons in fasterDB.
 
     :param cnx: (sqlite3 object) allow connection to sed database
     :param exon_type: (string) the type of control exon we want to use
+    :param exon2remove: (list of 2 int) the exons we want to remove
     :return:
         * result: (list of tuple) every information about control exons
         * names: (list of string) the name of every column in sed table
@@ -22,20 +25,20 @@ def get_control_exon_information(cnx, exon_type):
     cursor = cnx.cursor()
     if exon_type != "ALL":
         query = """SELECT t2.official_symbol, t1.id_gene, t1.pos_on_gene
-                   FROM exons t1, genes t2
-                   WHERE t1.exon_type LIKE '%{}%'
-                   AND t1.id_gene = t2.id""".format(exon_type)
+                    FROM exons t1, genes t2
+                    WHERE t1.exon_type LIKE '%{}%'
+                    AND t1.id_gene = t2.id""".format(exon_type)
     else:
         query = """SELECT t2.official_symbol, t1.id_gene, t1.pos_on_gene
-                   FROM exons t1, genes t2
-                   AND t1.id_gene = t2.id
-                """
+                    FROM exons t1, genes t2
+                    AND t1.id_gene = t2.id
+                 """
     cursor.execute(query)
     result = cursor.fetchall()
     # turn tuple into list
-    nresult = []
-    for exon in result:
-        nresult.append(list(exon))
+    print("number of control exon before removing bad ones : %s" % len(result))
+    nresult = [list(exon) for exon in result if [exon[1], exon[2]] not in exon2remove]
+    print("number of control exon after removing bad ones : %s" % len(nresult))
     return nresult
 
 
@@ -52,13 +55,16 @@ def control_dictionaries_creator(window_size):
     exon_class.set_debug(0)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     fasterdb = os.path.dirname(os.path.realpath(__file__)).replace("src/metaexon_figure", "data/fasterDB_lite.db")
+    seddb = os.path.dirname(os.path.realpath(__file__)).replace("src/metaexon_figure", "data/sed.db")
     ctrl_dir = dir_path + "/control_dictionaries/"
     cnx = sqlite3.connect(fasterdb)
+    cnx_sed = sqlite3.connect(seddb)
     if not os.path.isdir(ctrl_dir):
         os.mkdir(ctrl_dir)
+    exon2remove = union_dataset_function.get_exon_regulated_by_sf(cnx_sed, "down")
     exon_type = ["ACE", "CCE"]
     for cur_exon_type in exon_type:
-        ctrl_exon_list = get_control_exon_information(cnx, cur_exon_type)
+        ctrl_exon_list = get_control_exon_information(cnx, cur_exon_type, exon2remove)
         # ctrl_exon_list = ctrl_exon_list[0:2]
         list_exon = [exon_class.ExonClass(cnx, exon[0], exon[1], exon[2], window_size) for exon in ctrl_exon_list]
         print("creating metagene windowsed")

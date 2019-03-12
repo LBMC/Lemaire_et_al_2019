5d3fa18d1d5245182dc5e86378ec5d9d4a85a29d
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-03-12 10:18:25 +0100

    src/make_control_files_bp_ppt/stat_bp.py : creation of glm_nb_stats_spliceosome function used in strecth analysis

--- GC_rich_AT_rich_exon_list/src/make_control_files_bp_ppt/stat_bp.py
+++ GC_rich_AT_rich_exon_list/src/make_control_files_bp_ppt/stat_bp.py
@@ -40,6 +40,37 @@ def glm_poisson_stats(dataframe, filename):
     return df_stats
 
 
+def glm_poisson_stats_spliceosome(dataframe, filename):
+    """
+    Perform a glm nb test on ``list_values1`` and ``list_values2``.
+
+    :param dataframe: (pandas DataFrame) a dataframe
+    :param filename: (string)  list of float
+    :return: (float) the pvalue of the mann withney test done one `list_values1`` and ``list_values2``.
+    """
+    dataframe = dataframe[(dataframe["project"] != "CCE_exons") & (dataframe["project"] != "CCE")]
+    r_df = pandas2ri.py2ri(dataframe)
+    stat_s = r("""
+    require("DHARMa")
+    require("multcomp")
+    function(data, name){
+        factors <- levels(as.factor(as.vector(data$project)))
+        data$project <- as.factor(as.vector(data$project))
+        mlm <- glm(values ~ project, data=data, family="poisson")
+        simulationOutput <- simulateResiduals(fittedModel = mlm, n = 250)
+        png(paste(name, "_dignostics.png", sep=""), height=1080, width=1920)
+        par(mfrow=c(2, 2))
+        plot(simulationOutput)
+        dev.off()
+        return(as.data.frame(summary(mlm)$coefficients))
+    }
+               """)
+    name = filename.split(".")[0]
+    df_stats = pandas2ri.ri2py(stat_s(r_df, name))
+    df_stats["project"] = df_stats.index
+    return df_stats
+
+
 def glm_nb_stats(dataframe, filename):
     """
     Perform a glm nb test on ``list_values1`` and ``list_values2``.
@@ -70,4 +101,35 @@ def glm_nb_stats(dataframe, filename):
                """)
     name = filename.split(".")[0]
     df_stats = pandas2ri.ri2py(stat_s(r_df, name))
+    return df_stats
+
+
+def glm_nb_stats_spliceosome(dataframe, filename):
+    """
+    Perform a glm nb test on ``list_values1`` and ``list_values2``.
+
+    :param dataframe: (pandas DataFrame) a dataframe
+    :param filename: (string)  list of float
+    :return: (float) the pvalue of the mann withney test done one `list_values1`` and ``list_values2``.
+    """
+    dataframe = dataframe[(dataframe["project"] != "CCE_exons") & (dataframe["project"] != "CCE")]
+    r_df = pandas2ri.py2ri(dataframe)
+    stat_s = r("""
+    require("DHARMa")
+    require("multcomp")
+    function(data, name){
+        factors <- levels(as.factor(as.vector(data$project)))
+        data$project <- as.factor(as.vector(data$project))
+        mlm <- glm.nb(values ~ project, data=data)
+        simulationOutput <- simulateResiduals(fittedModel = mlm, n = 250)
+        png(paste(name, "_dignostics.png", sep=""), height=1080, width=1920)
+        par(mfrow=c(2, 2))
+        plot(simulationOutput)
+        dev.off()
+        return(as.data.frame(summary(mlm)$coefficients))
+    }
+               """)
+    name = filename.split(".")[0]
+    df_stats = pandas2ri.ri2py(stat_s(r_df, name))
+    df_stats["project"] = df_stats.index
     return df_stats

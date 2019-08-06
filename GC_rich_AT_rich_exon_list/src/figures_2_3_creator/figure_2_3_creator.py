017f8dab05dda31e2bac7ac70cba434ba2bcef4b
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-06 12:43:19 +0200

    src/figures_2_3_creator/figure_2_3_creator.py : addition of the function main_2a that allow to create the figure 2A

--- GC_rich_AT_rich_exon_list/src/figures_2_3_creator/figure_2_3_creator.py
+++ GC_rich_AT_rich_exon_list/src/figures_2_3_creator/figure_2_3_creator.py
@@ -14,12 +14,14 @@ import lazyparser as lp
 base1 = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, base1 + "/boxplot_GC_content_and_flanking_intron_size/")
 from launcher import main_1d
+sys.path.insert(0, base1 + "/metaexon_figure/")
+from launcher_metaexon import main as main_2a
 
 
 @lp.parse(list_file="file", seddb="file", exon_type=["CCE", "ALL"],
           regulation=["up", "down"], output="dir")
-def figure_creator(list_file, name_file, seddb, output, exon_type="CCE",
-                   regulation="down", nt="S"):
+def figure_creator(list_file, name_file, seddb, fasterdb, output,
+                   exon_type="CCE", regulation="down", nt="S"):
     """
     Create the figure 2 and 3 with custom list of exons.
 
@@ -38,6 +40,9 @@ def figure_creator(list_file, name_file, seddb, output, exon_type="CCE",
         output += "/"
     print("Creating figure 1D...")
     main_1d(list_file, name_file, seddb, exon_type, regulation, output, nt)
+    print("Creation of figure 2A...")
+    main_2a(list_file, name_file, "C,S,A,T,G,W,Y,R".split(","), "2A_metaexon",
+            exon_type, ["#0000FF", "#00aa00"], False, output, fasterdb)
 
 
 if __name__ == "__main__":

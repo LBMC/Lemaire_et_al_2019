9a27175a641043a71ddbb6b88b763caa1b95deac
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-06 12:43:40 +0200

    add src/figures_2_3_creator/command_figure2_3.sh : update

--- GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
+++ GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
@@ -2,6 +2,6 @@
 
 mkdir result/figure_2_3
 mkdir result/figure_2_3/test
-python3.5 src/figures_2_3_creator/figure_2_3_creator.py -l result/GC_rich_exons result/AT_rich_exons -n GC-exons AT-exons -s data/sed.db -o result/figure_2_3/test
+python3.5 src/figures_2_3_creator/figure_2_3_creator.py -l result/GC_rich_exons result/AT_rich_exons -n GC-exons AT-exons -s data/sed.db -f data/fasterDB_lite.db -o result/figure_2_3/test
 
 

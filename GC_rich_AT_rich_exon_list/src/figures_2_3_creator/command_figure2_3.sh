dcbc6c77d889237e0adbef51c08baa3ded1d984c
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-07 11:51:37 +0200

    src/figures_2_3_creator/command_figure2_3.sh : update

--- GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
+++ GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
@@ -2,6 +2,6 @@
 
 mkdir result/figure_2_3
 mkdir result/figure_2_3/test
-python3.5 src/figures_2_3_creator/figure_2_3_creator.py -l result/GC_rich_exons result/AT_rich_exons -n GC-exons AT-exons -s data/sed.db -f data/fasterDB_lite.db -o result/figure_2_3/test -S 'gc_rich_down' 'at_rich_down'
+python3.5 src/figures_2_3_creator/figure_2_3_creator.py -l result/GC_rich_exons result/AT_rich_exons -n GC-exons AT-exons -s data/sed.db -f data/fasterDB_lite.db -o result/figure_2_3/test -b ../Clip_analysis/data/coverage_project_selected/ -r ../Clip_analysis/data/hg19.ren.chrom.sizes -m /media/nicolas/DD_1/Splicing_Lore_project/FarLine_exons_results_summary/src/skipped_exon_list_results_summary/coverage_summary/metagene_coverage.r -S 'gc_rich_down' 'at_rich_down'
 
 

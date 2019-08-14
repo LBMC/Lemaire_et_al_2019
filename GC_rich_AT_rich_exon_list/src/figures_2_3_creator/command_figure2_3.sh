0a8bcb1dd5265f9fa81ebf8d309ad78eb966b9ef
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-14 10:16:37 +0200

    src/figures_2_3_creator/command_figure2_3.sh : update

--- GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
+++ GC_rich_AT_rich_exon_list/src/figures_2_3_creator/command_figure2_3.sh
@@ -8,6 +8,7 @@ python3.5 src/figures_2_3_creator/figure_2_3_creator.py -l result/GC_rich_exons 
 
 mkdir result/figure_2_3/GA_CT_figures
 mkdir result/figure_2_3/otherGC_GC_figures
+mkdir result/figure_2_3/otherGC_AT_figures
 mkdir result/figure_2_3/unregulated_GC_AT_exons
 
 python3.5 src/figures_2_3_creator/figure_2_3_creator.py \
@@ -24,7 +25,7 @@ python3.5 src/figures_2_3_creator/figure_2_3_creator.py \
 
 
 python3.5 src/figures_2_3_creator/figure_2_3_creator.py \
--l result/GC_rich_exons result/figure_2_3/exon_list/other_GC_rich_exons.txt \
+-l result/figure_2_3/exon_list/GC_rich_exons.txt result/figure_2_3/exon_list/other_GC_rich_exons.txt \
 -n GC-exons otherGC-exons \
 -s data/sed.db \
 -f data/fasterDB_lite.db \
@@ -33,6 +34,18 @@ python3.5 src/figures_2_3_creator/figure_2_3_creator.py \
 -r ../Clip_analysis/data/hg19.ren.chrom.sizes \
 -m /media/nicolas/DD_1/Splicing_Lore_project/FarLine_exons_results_summary/src/skipped_exon_list_results_summary/coverage_summary/metagene_coverage.r \
 -S 'gc_rich_down' 'other'
+
+
+python3.5 src/figures_2_3_creator/figure_2_3_creator.py \
+-l result/figure_2_3/exon_list/other_GC_rich4ATcomp_exons.txt result/figure_2_3/exon_list/AT_rich_exons.txt \
+-n otherGC-exons2 AT-exons \
+-s data/sed.db \
+-f data/fasterDB_lite.db \
+-o result/figure_2_3/otherGC_AT_figures \
+-b ../Clip_analysis/data/coverage_project_selected/ \
+-r ../Clip_analysis/data/hg19.ren.chrom.sizes \
+-m /media/nicolas/DD_1/Splicing_Lore_project/FarLine_exons_results_summary/src/skipped_exon_list_results_summary/coverage_summary/metagene_coverage.r \
+-S 'other' 'at_rich_down'
 
 
 python3.5 src/figures_2_3_creator/figure_2_3_creator.py \

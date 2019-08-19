a98281e9924b28f0c4fed98b4cf04e38cb7c70bf
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-19 12:50:25 +0200

    src/figure_creator.py: creation of venn3_diagram_creator + modification of venn_diagram_creator and main function to produce graphics with the wanted color

--- GC_rich_AT_rich_exon_list/src/figure_creator.py
+++ GC_rich_AT_rich_exon_list/src/figure_creator.py
@@ -14,7 +14,7 @@ import union_dataset_function
 import sqlite3
 import os
 import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
+from matplotlib_venn import venn2, venn3
 import group_factor
 
 
@@ -37,21 +37,25 @@ def get_exons_list(cnx, sf_list, regulation):
     return exon_list
 
 
-def venn_diagram_creator(list1, name1, list2, name2, output):
+def venn_diagram_creator(list1, name1, color1, list2, name2, color2, output):
     """
     Create a venn diagram of the `list1` and `list2` lists of values.
 
     :param list1: (list of 2 int) list of exons identified by their gene_id and exon_position within the hosting gene
     :param name1: (string) the name of the list of exons named `list1`
+    :param color1: (string) the color of list1
     :param list2: (list of 2 int) list of exons identified by their gene_id and exon_position within the hosting gene
     :param name2: (string) the name of the list of exons named `list2`
+    :param color2: (string) the color of list 2
     :param output: (string) path where the result venn diagram will be created
     :return:
     """
     flat_list1 = ["%s_%s" % (exon[0], exon[1]) for exon in list1]
     flat_list2 = ["%s_%s" % (exon[0], exon[1]) for exon in list2]
     # plt.figure(figsize=(48. / 2.54, 27 / 2.54))
-    venn2([set(flat_list1), set(flat_list2)], set_labels=(name1, name2))
+    c = venn2([set(flat_list1), set(flat_list2)], set_labels=(name1, name2))
+    c.get_patch_by_id('10').set_color(color1)
+    c.get_patch_by_id('01').set_color(color2)
     plt.savefig("%sVenn_%s_vs_%s.pdf" % (output, name1, name2))
     plt.clf()
     plt.cla()
@@ -89,8 +93,32 @@ def file_writer(list_exon, name_list, output):
             my_file.write("%s\t%s\n" % (exon[0], exon[1]))
 
 
+def venn3_diagram_creator(list_exons, list_names, list_color, output):
+    """
+    Create a venn diagram of the `list1` and `list2` lists of values.
+
+    :param list_exons: (list of list of 2 int) list of exons
+    :param list_names: (list of string) the names of the list of exons
+    :param output: (string) path where the result venn diagram will be created
+    :return:
+    """
+    def flaten_list(x): return ["%s_%s" % (a[0], a[1]) for a in x]
+    new_lists = [set(flaten_list(my_list)) for my_list in list_exons]
+    c = venn3(new_lists, set_labels=list_names)
+    c.get_patch_by_id('100').set_color(list_color[0])
+    c.get_patch_by_id('010').set_color(list_color[1])
+    c.get_patch_by_id('001').set_color(list_color[2])
+    plt.savefig("%sVenn_%s.pdf" % (output, "_".join(list_names)))
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+
 def main():
-    couples = [["AT-exons", "GC-exons"], ["AT-exons_U2", "GC_rich_U1"], ["U1-exons", "U2-exons"], ["GC-exons", "U1-exons"], ["AT-exons", "U1-exons"], ["GC-exons", "U2-exons"], ["AT-exons", "U2-exons"]]
+    color_dic2 = {"GC-exons": "#5555FF", "AT-exons": "#00aa00",
+                 "U1-exons": "#7777F0", "U2-exons": "#33EE33",
+                 "AT-exons_U2": "#aaFFaa", "GC_rich_U1": "#aaaaFF"}
+    couples = [["AT-exons", "GC-exons"], ["U1-exons", "U2-exons"], ["GC-exons", "U1-exons"], ["AT-exons", "U1-exons"], ["GC-exons", "U2-exons"], ["AT-exons", "U2-exons"]]
     seddb = os.path.realpath(os.path.dirname(__file__)).replace("src", "data/sed.db")
     cnx = sqlite3.connect(seddb)
     output = os.path.realpath(os.path.dirname(__file__)).replace("src", "result/")
@@ -105,13 +133,26 @@ def main():
         dic_exon[name_group] = exon_list
     # for key in dic_exon.keys():
     #     file_writer(dic_exon[key], "%s_with_intersection" % key, output)
-    dic_exon["GC-exons"] = [exon for exon in dic_exon["GC-exons-all"] if exon not in dic_exon["AT-exons-all"]]
-    dic_exon["AT-exons"] = [exon for exon in dic_exon["AT-exons-all"] if exon not in dic_exon["GC-exons-all"]]
+    dic_exon["GC-exons"] = [exon for exon in dic_exon["GC-exons-all"]
+                            if exon not in dic_exon["AT-exons-all"]]
+    dic_exon["AT-exons"] = [exon for exon in dic_exon["AT-exons-all"]
+                            if exon not in dic_exon["GC-exons-all"]]
     print("GC-exons : %s exons" % len(dic_exon["GC-exons"]))
     print("AT-exons : %s exons" % len(dic_exon["AT-exons"]))
     for couple in couples:
-        venn_diagram_creator(dic_exon[couple[0]], couple[0], dic_exon[couple[1]], couple[1], output)
-        intersection_supresser_and_file_writer(dic_exon[couple[0]], couple[0], dic_exon[couple[1]], couple[1], output)
+        venn_diagram_creator(dic_exon[couple[0]], couple[0], color_dic2[couple[0]],
+                             dic_exon[couple[1]], couple[1], color_dic2[couple[1]], output)
+        intersection_supresser_and_file_writer(dic_exon[couple[0]], couple[0],
+                                               dic_exon[couple[1]], couple[1],
+                                               output)
+    list_name1 = ["U1-exons", "AT-exons", "GC-exons"]
+    list_color = [color_dic2[mylist] for mylist in list_name1]
+    list_exon = [dic_exon[mylist] for mylist in list_name1]
+    venn3_diagram_creator(list_exon, list_name1, list_color, output)
+    list_name2 = ["U2-exons", "AT-exons", "GC-exons"]
+    list_color = [color_dic2[mylist] for mylist in list_name2]
+    list_exon = [dic_exon[mylist] for mylist in list_name2]
+    venn3_diagram_creator(list_exon, list_name2, list_color, output)
     cnx.close()
 
 

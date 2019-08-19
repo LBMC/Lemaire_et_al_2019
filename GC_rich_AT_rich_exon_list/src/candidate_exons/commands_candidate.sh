74df747d6ef90e0c504dcd289b36a0c5238de8c9
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-08-19 12:48:13 +0200

    src/candidate_exons/commands_candidate.sh : update

--- GC_rich_AT_rich_exon_list/src/candidate_exons/commands_candidate.sh
+++ GC_rich_AT_rich_exon_list/src/candidate_exons/commands_candidate.sh
@@ -4,6 +4,8 @@ mkdir result/candidate
 mkdir result/candidate/GC
 python3.5 src/candidate_exons/GC_candidate_exons.py -e result/GC_rich_exons -n GC -l SNRNP70 SNRPC DDX5_DDX17 -s data/sed.db -f data/fasterDB_lite.db -o result/candidate/GC
 python3.5 src/candidate_exons/GC_candidate_exons.py -e result/GC_rich_exons -n GC -l SNRPC DDX5_DDX17 -s data/sed.db -f data/fasterDB_lite.db -o result/candidate/GC
+python3.5 src/candidate_exons/GC_candidate_exons.py -e result/GC_rich_exons -n GC -l SNRNP70 DDX5_DDX17 -s data/sed.db -f data/fasterDB_lite.db -o result/candidate/GC
+python3.5 src/candidate_exons/GC_candidate_exons.py -e result/GC_rich_exons -n GC -l SNRNP70 SNRPC -s data/sed.db -f data/fasterDB_lite.db -o result/candidate/GC
 
 mkdir result/candidate/AT
 python3.5 src/candidate_exons/GC_candidate_exons.py -e result/AT_rich_exons -n AT -l U2AF2 SF1 -s data/sed.db -f data/fasterDB_lite.db -o result/candidate/AT --ss "3'ss"
